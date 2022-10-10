95dcbc5201986110261a9959ac34393995bfb580
Oliveira <biel07097@gmail.com>
2022-10-10 17:02:22 -0300

    adicionandop operadores aritméticos básicos

---
+++ js/Soma.js
@@ -0,0 +1,35 @@
+function soma(){
+    var x = 2
+    var y = 2
+    let resultado = x+y
+
+    document.getElementById("resultado").innerHTML=resultado
+    document.getElementById("confirmado-ad").innerHTML="resultado✔"
+}
+
+function multiplicacao(){
+    var x = 2 
+    var y = 2 
+    let resultado = x*y
+
+    document.getElementById("resultado2").innerHTML=resultado
+    document.getElementById("confirmado-mu").innerHTML="resultado✔"
+}
+
+function subtracao(){
+    var x = 5
+    var y = 3
+    let resultado = x-y
+
+    document.getElementById("resultado3").innerHTML=resultado
+    document.getElementById("confirmado-sub").innerHTML="resultado✔"
+}
+    
+function divisao(){
+    var x = 8
+    var y = 2
+    let resultado = x/y
+
+    document.getElementById("resultado4").innerHTML=resultado
+    document.getElementById("confirmado-div").innerHTML="resultado✔"
+}
