3764f4be1165c2dda867c1b0378a0208fe0777f0
Oliveira <biel07097@gmail.com>
2022-10-27 17:54:19 -0300

    Adicionando links e style na página home e aulas

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import StyleEstilo from "style/estilo.css"
+import StyleBotao from 'Style/botao.css'
 
 export default function Home() {
   return (
@@ -7,18 +9,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Prof Gustavo Ferreira" />
         <meta name="descripion" content="Página inicial de Desenvolvimento WEB" />
-        <title>Curso de Desenvolvimento WEB</title>
-        <link rel="stylesheet" href="css/estilo.css" type="text/css" /> 
+        <title>Curso de Desenvolvimento WEB</title> 
         <link rel="stylesheet" href="css/botao.css" type="text/css" />
         <link rel="icon" href="./icon.png" type="image/x-icon" />
-        <div className="container">
-          <div className="conteudo cinza" id="arredondar">
-            <Link href="microcamp.com.br" className="cinza">
-              <img className="cinza" id="arredondar" src="/logo.jpg" type="jpg" alt="Logo Microcamp" />
+        <div className={StyleEstilo.container}>
+          <div className= {[StyleEstilo.conteudo, StyleEstilo.cinza] .join(" ")} id="arredondar">
+            <Link href="microcamp.com.br" className={StyleEstilo.cinza}>
+              <img className= {StyleEstilo.cinza} id="arredondar" src="/logo.jpg" type="jpg" alt="Logo Microcamp" />
             </Link>
-            <h1 className="cinza">Seja Bem-Vindo</h1>
-            <p className="cinza-escuro" id="altera">Clique em começar para aprender! 👇</p>
-            <Link className="botao" href="pages/aulas.js">Começar</Link>
+            <h1 className= {StyleEstilo.cinza} >Seja Bem-Vindo</h1>
+            <p className= {StyleEstilo.cinza-escuro} id="altera">Clique em começar para aprender! 👇</p>
+            <Link className={StyleBotao.botao} href="pages/aulas.js">Começar</Link>
           </div>
         </div>
       </div>
--- pages/pages/aulas.js
+++ pages/pages/aulas.js
@@ -1,6 +1,9 @@
-import Link  from 'next/link'
+import Link from 'next/link'
+import StyleEstilo from "style/estilo.css"
+import StyleBotao from 'Style/botao.css'
+import StyleTabela from 'Style/tabela.css'
 
-export default function Home() {
+export default function Aulas() {
   return (
     <div>
         <meta charSet="utf-8" />
@@ -15,13 +18,13 @@ export default function Home() {
         <link rel="stylesheet" href="../css/botao.css" type="text/css" />
         <link rel="icon" href="./Icon.png" type="image/x-icon" />
         <div className="container">
-          <div className="conteudo cinza" id="arredondar">
-            <Link href="microcamp.com.br" className="cinza">
-              <img src="/logo.jpg" alt="Logo Microcamp" className="cinza" id="arredondar" />
+          <div className= {[StyleEstilo.cinza, StyleEstilo.container].join(" ")} id="arredondar">
+            <Link href="microcamp.com.br" className= {StyleEstilo.cinza}>
+              <img src="/logo.jpg" alt="Logo Microcamp" className= {StyleEstilo.cinza}id="arredondar" />
             </Link>
-            <h1 className="cinza">Bem vindo ao Curso de Programação! 💻</h1>
-            <p className="cinza-escuro">Clique na aula que deseja:</p> 
-            <table id="tabela">
+            <h1 className= {StyleEstilo.cinza}>Bem vindo ao Curso de Programação! 💻</h1>
+            <p className= {StyleEstilo.cinza-escuro}>Clique na aula que deseja:</p> 
+            <table className= {StyleTabela.tabela} >
               <tbody><tr>
                   <th>Aula</th>
                   <th>Modulo</th>
@@ -29,42 +32,42 @@ export default function Home() {
                 </tr>
                 <tr>
                   <td>
-                    <Link className="botao" href="aulas/aula-01.html">01</Link>
+                    <Link className= {StyleBotao.botao} href="/aulas/aula-01">01</Link>
                   </td>
                   <td>HTML</td>
                   <td>Iniciando um Projeto WEB</td>
                 </tr>
                 <tr>
                   <td>
-                    <Link className="botao" href="aulas/aula-02.html">02</Link>
+                    <Link className= {StyleBotao.botao} href="/aulas/aula-02">02</Link>
                   </td>
                   <td>CSS3</td>
                   <td>Vinculando o CSS3 Externo</td>
                 </tr>
                 <tr>
                   <td>
-                    <Link className="botao" href="aulas/aula-03.html">03</Link>
+                    <Link className= {StyleBotao.botao} href="/aulas/aula-03">03</Link>
                   </td>
                   <td>JavaScript</td>
                   <td>Alterando template com CSS3</td>
                 </tr>
                 <tr>
                   <td>
-                    <Link className="botao" href="aulas/aula-04.html">04</Link>
+                    <Link className= {StyleBotao.botao} href="/aulas/aula-04">04</Link>
                   </td>
                   <td>BootStrap</td>
                   <td>Implementando bootstrap</td>
                 </tr>
                 <tr>
                   <td>
-                    <Link className="botao" href="aulas/aula-05.html">05</Link>
+                    <Link className= {StyleBotao.botao} href="/aulas/aula-05">05</Link>
                   </td>
                   <td>JavaScript</td>
                   <td>Operadores em JavaScript</td>
                 </tr>
                 <tr>
                   <td>
-                    <Link className="botao" href="aulas/aula-06.html">06</Link>
+                    <Link className= {StyleBotao.botao} href="/aulas/aula-06">06</Link>
                   </td>
                   <td>JavaScript</td>
                   <td>Portas lógicas JavaScript</td>
