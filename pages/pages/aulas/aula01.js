d97a32046daa8cb761ecfadd50c462aefbdab429
Oliveira <biel07097@gmail.com>
2022-11-14 17:08:42 -0300

    Corrindo erros

--- pages/pages/aulas/aula01.js
+++ pages/pages/aulas/aula01.js
@@ -3,7 +3,7 @@ import styleEstilo from '../../../styles/estilo.module.css'
 import styleBotao from '../../../styles/botao.module.css'
 import styleLista from '../../../styles/lista.module.css'
 import styleTab from '../../../styles/tabela-clara.module.css'
-import styleBoot from 'bootstrap/dist/css/bootstrap.min.css'
+
 
 export default function aula01(){
     return(
@@ -15,10 +15,9 @@ export default function aula01(){
         <title>Aula 01 - Git com HTML e CSS</title>
         <link rel="icon" href="/icon.png" type="image/x-icon" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
         <header>
-          /***/
           <nav className="navbar navbar-expand-sm, bg-dark, navbar-dark">
-          
             <div className="container-fluid">
             
               <ul className="navbar-nav container">
@@ -30,15 +29,14 @@ export default function aula01(){
                   </Link>
                 </li>
               
-                <li className={[styleBoot.nav-item, styleBoot.text-white].join(" ")}>
+                <li className="nav-item text-white">
                   <h4>Aula 01 - Git com HTML e CSS</h4>
                 </li>
               </ul>
             </div>
           </nav>
         </header>
-        /***/
-        <section className={styleBoot.row}>
+        <section className="row">
           <div className="col-lg-3 bg-purple">
           <nav className={styleLista.nav}>
             <ul className="navbar-nav m-3">
